c62311baa86e21a3fb99e19c887374557feaa92f
Rivera-py <jaluri@outlook.com>
2025-08-09 15:54:46 +0100

    Add cluster config

--- aws/cluster.tf
+++ aws/cluster.tf
@@ -1,4 +1,74 @@
-# resource "aws_eks_cluster" "cluster" {
-#   name     = "eks-cluster-${var.environment}"
-#   role_arn = aws_iam_role.eks_service_role.arn
-# }
+# EKS Cluster Requires Resources
+
+## EKS Encryption Key
+
+data "aws_iam_policy_document" "eks_secrets_kms" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+    actions   = ["kms:*"]
+    resources = ["*"]
+  }
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:DescribeKey"
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_kms_key" "eks_secrets" {
+  description         = "KMS key for EKS secrets encryption - Managed by Terraform"
+  enable_key_rotation = true
+  policy              = data.aws_iam_policy_document.eks_secrets_kms.json
+
+  tags = {
+    Name = "${var.environment}-eks-secrets-kms"
+  }
+}
+
+
+# EKS Cluster Configuration
+
+resource "aws_eks_cluster" "cluster" {
+  name                      = "eks-cluster-${var.environment}"
+  role_arn                  = aws_iam_role.eks_service_role.arn
+  enabled_cluster_log_types = var.control_plane_log_types
+  version                   = var.kubernetes_version
+
+  vpc_config {
+    public_access_cidrs = [var.allowed_public_ingress_ip]
+    subnet_ids          = aws_subnet.eks_private[*].id
+  }
+
+  encryption_config {
+    resources = ["secrets"]
+    provider {
+      key_arn = aws_kms_key.eks_secrets.arn
+    }
+  }
+
+  kubernetes_network_config {
+    service_ipv4_cidr = var.eks_service_cidr
+  }
+
+  upgrade_policy {
+    support_type = "STANDARD"
+  }
+
+  depends_on = [
+    aws_cloudwatch_log_group.eks_control_plane
+  ]
+}
--- aws/variables.tf
+++ aws/variables.tf
@@ -10,6 +10,7 @@ variable "environment" {
 }
 
 # Variables for EKS Network Resources
+
 variable "vpc_cidr" {
   description = "CIDR block for the EKS VPC."
   type        = string
@@ -50,4 +51,30 @@ variable "allowed_public_ingress_ip" {
   description = "The public IP allowed to access the public subnet on port 443."
   type        = string
   default     = "198.51.100.10/32" # Example IP, replace as needed
+}
+
+# Variables for EKS Cluster
+
+variable "control_plane_log_types" {
+  description = "List of control plane log types to enable for the EKS cluster. Can include: api, audit, authenticator, controllerManager, scheduler."
+  type        = list(string)
+  default = [
+    "api",
+    "audit",
+    "authenticator",
+    "controllerManager",
+    "scheduler"
+  ]
+}
+
+variable "eks_service_cidr" {
+  description = "CIDR block for the EKS service network."
+  type        = string
+  default     = "192.168.0.0/16"
+}
+
+variable "kubernetes_version" {
+  description = "Kubernetes version for the EKS cluster."
+  type        = string
+  default     = null
 }
